6f4478950869990b16de0d59b4f7021e387b4ff5
AjayGhode <ajaykghode11@gmail.com>
2022-11-17 15:14:08 +0530

    Second changes

--- ArraySort/src/Sort.java
+++ ArraySort/src/Sort.java
@@ -27,9 +27,27 @@ public class Sort {
 		        System.out.println();    
 		            
 		        //Displaying elements of array after sorting    
-		        System.out.println("Elements of array sorted in ascending order: ");    
+		        System.out.println("Elements of array sorted in ASCENDING order: ");    
 		        for (int i = 0; i < arr.length; i++) {     
 		            System.out.print(arr[i] + " ");    
-		        }    
-		    }    
-		}    
+		        }   
+		        
+		        for (int i = 0; i < arr.length; i++) {     
+		            for (int j = i+1; j < arr.length; j++) {     
+		               if(arr[i] < arr[j]) {    
+		                   temp = arr[i];    
+		                   arr[i] = arr[j];    
+		                   arr[j] = temp;    
+		               }     
+		            }     
+		    }   
+		        System.out.println();
+		        //Displaying elements of array after sorting
+		        System.out.println("Elements of array sorted in DESCENDING order : ");
+		        for(int i = 0; i < arr.length;i++)
+		        {
+		        	System.out.print(arr[i]+" ");
+		        	System.out.println("Thank You !!");
+		        }
+		 }
+}
